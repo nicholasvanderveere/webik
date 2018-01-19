15d8e7e006d415fc0c1d5ac68ed4052dd4375415
Nicholas Van Der Veere <nicholas.nvdv@gmail.com>
2018-01-19 11:41:48 +0000

    First steps

---
+++ application.py
@@ -0,0 +1,123 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.route("/")
+@login_required
+def index():
+    return apology("TODO")
+
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions."""
+    return apology("TODO")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # test username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # test password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+        # query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # test username exists and password is correct
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+            return apology("invalid username and/or password")
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("login"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
+    if request.method == "POST":
+
+        # test username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # test password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+         # test password is the same
+        elif request.form.get("password") != request.form.get("confirm_password"):
+            return apology("password doesn't match")
+
+        # insert user into database with a hash value
+        result = db.execute("INSERT INTO users (username, hash) \
+                            VALUES (:username, :hasha)", \
+                            username = request.form.get("username"), \
+                            hasha = pwd_context.hash(request.form.get("password")))
+
+        if not result:
+            return apology("The Username was empty or the Username already exists")
+
+        session["user_id"] = result
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    else:
+        return render_template("register.html")
